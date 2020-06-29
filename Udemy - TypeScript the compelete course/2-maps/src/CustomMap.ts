3146f0c7c178b5243574a74ec025ee9bc3f34cdc
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-29 12:45:44 +0000

    Adding CustomMap.ts script, 2-maps

---
+++ Udemy - TypeScript the compelete course/2-maps/src/CustomMap.ts
@@ -0,0 +1,30 @@
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  }
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });    
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
+  }
+}
