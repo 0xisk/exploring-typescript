a32603ae289dbc3276c921739cd686eb1874a482
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-29 12:43:16 +0000

    Adding functions.ts script, 1-features

---
+++ Udemy - TypeScript the compelete course/1-features/annotations/functions.ts
@@ -0,0 +1,16 @@
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+function divide(a: number, b: number) {
+  return a + b; 
+};
+
+const logger = (msg: string) => {
+  console.log(msg);
+};
+
+const throwError = (msg: string) => {
+  throw new Error(msg);
+};
+
