41eee9cad7f82cd47d97ea2485d195a595645a5c
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-01 17:52:40 +0000

    Implementing bubble sort algorithm with strings and see how TypeScript deal with it, 2-sort.

--- Udemy - TypeScript the compelete course/3-sort/src/index.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/index.ts
@@ -8,6 +8,7 @@
 } */
 
 // New JS 
+// For Numbers
 class Sorter {
    constructor(public collection: number[]) {}
 
@@ -29,3 +30,32 @@ class Sorter {
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
+
+
+// For strings
+/* class Sorter {
+   constructor(public collection: number[] | string) {}
+
+   sort(): void {
+     const { length } = this.collection;
+
+     for (let i = 0; i < length; i++) {
+       for (let j = 0; j < length - i - 1; j++) {
+        // If collection is an array of numbers
+         if (this.collection[j] > this.collection[j+1]) {
+           const temp = this.collection[j];
+           this.collection[j] = this.collection[j+1];
+           this.collection[j+1] = temp;
+         }
+
+         // If collection is string do this logic instead: 
+         // ~~~logic to compare and swap characters in a string
+
+       }
+     }
+   }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection); */
