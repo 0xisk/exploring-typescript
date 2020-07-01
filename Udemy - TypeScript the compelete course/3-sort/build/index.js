19569b57dea7857045668d04bcfb1b2af2666ab7
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-01 17:32:06 +0000

    Finished implementing bubble sort algorithm version-1, 2-sort.

--- Udemy - TypeScript the compelete course/3-sort/build/index.js
+++ Udemy - TypeScript the compelete course/3-sort/build/index.js
@@ -13,6 +13,16 @@ var Sorter = /** @class */ (function () {
         this.collection = collection;
     }
     Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
     };
     return Sorter;
 }());
--- Udemy - TypeScript the compelete course/3-sort/src/index.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/index.ts
@@ -12,7 +12,17 @@ class Sorter {
    constructor(public collection: number[]) {}
 
    sort(): void {
+     const { length } = this.collection;
 
+     for (let i = 0; i < length; i++) {
+       for (let j = 0; j < length - i - 1; j++) {
+         if (this.collection[j] > this.collection[j+1]) {
+           const temp = this.collection[j];
+           this.collection[j] = this.collection[j+1];
+           this.collection[j+1] = temp;
+         }
+       }
+     }
    }
 }
 
