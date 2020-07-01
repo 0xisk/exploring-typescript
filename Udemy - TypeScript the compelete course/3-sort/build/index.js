eb19548f9068fa60c8c32ab2e01e51d452ffdee3
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-01 17:23:02 +0000

    Implementing a sorter class and a sort method for bubble sorting algorithm, 2-sort.

--- Udemy - TypeScript the compelete course/3-sort/build/index.js
+++ Udemy - TypeScript the compelete course/3-sort/build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('Hi there!');
+// Old JS
+/* class Sorter {
+   collection: number[];
+
+   constructor(collection: number[]) {
+     this.collection = collection;
+   }
+} */
+// New JS 
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- Udemy - TypeScript the compelete course/3-sort/src/index.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/index.ts
@@ -1,2 +1,21 @@
-console.log('Hi there!');
- 
+// Old JS
+/* class Sorter {
+   collection: number[];
+
+   constructor(collection: number[]) {
+     this.collection = collection;
+   }
+} */
+
+// New JS 
+class Sorter {
+   constructor(public collection: number[]) {}
+
+   sort(): void {
+
+   }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
