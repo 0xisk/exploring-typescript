7c459980edb32f6c815fffae9dd6010a2b403c3a
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 12:45:20 +0000

    Successfully tested all classes with new abstract Sort class inheritance for sorting algorithm, 3-sort.

--- Udemy - TypeScript the compelete course/3-sort/build/index.js
+++ Udemy - TypeScript the compelete course/3-sort/build/index.js
@@ -1,25 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
 var LinkedList_1 = require("./LinkedList");
 var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-var sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
 /* const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data); */
 /* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
- */
+numbersCollection.sort();
+console.log(numbersCollection.data); */
 // Old JS
 /* class Sorter {
    collection: number[];
--- Udemy - TypeScript the compelete course/3-sort/src/index.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/index.ts
@@ -8,22 +8,17 @@ linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
 
 /* const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data); */
 
 /* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data); */
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
- */
 
 // Old JS
 /* class Sorter {
