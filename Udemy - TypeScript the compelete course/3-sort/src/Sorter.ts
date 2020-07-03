6c0802d1a56a12cc12a0f05c6eb5236ee3d21dd6
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 12:32:58 +0000

    Implementing an inheritance with NumbersCollection, CharactersCollection, LinkedList and Generalize Sort Class to fit all in sorting algorithm, 3-sort.

--- Udemy - TypeScript the compelete course/3-sort/src/Sorter.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/Sorter.ts
@@ -7,15 +7,13 @@ interface Sortable {
 } 
 
 export class Sorter {
-  constructor(public collection: Sortable) {}
-
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j+1)) {
-          this.collection.swap(j, j+1);
+        if (this.compare(j, j+1)) {
+          this.swap(j, j+1);
         }
       }
     }
