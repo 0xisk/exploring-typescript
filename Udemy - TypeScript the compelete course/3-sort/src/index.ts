71107fad3824d3056969c57ee4d87619cef13243
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 12:18:25 +0000

    Successfully tested LinkedList class for sorting algorithm, 3-sort.

--- Udemy - TypeScript the compelete course/3-sort/src/index.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/index.ts
@@ -1,11 +1,22 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollections';
 import { CharactersCollection } from  './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const charactersCollection = new CharactersCollection('Xaayb');
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
+
+/* const charactersCollection = new CharactersCollection('Xaayb');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(charactersCollection.data);
+console.log(charactersCollection.data); */
 
 /* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
