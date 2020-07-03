8832d228694ac8f117b4e6a32a60d9e1a1290440
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 12:03:13 +0000

    Implementing LinkedList class with add() and length() methods in sorting algorithm, 3-sort.

--- Udemy - TypeScript the compelete course/3-sort/build/LinkedList.js
+++ Udemy - TypeScript the compelete course/3-sort/build/LinkedList.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var Node = /** @class */ (function () {
     function Node(data) {
         this.data = data;
@@ -6,3 +7,38 @@ var Node = /** @class */ (function () {
     }
     return Node;
 }());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
+    }
+    LinkedList.prototype.add = function (data) {
+        var node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        var tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    };
+    Object.defineProperty(LinkedList.prototype, "length", {
+        get: function () {
+            if (!this.head) {
+                return 0;
+            }
+            var length = 1;
+            var node = this.head;
+            while (node.next) {
+                length++;
+                node = node.next;
+            }
+            return length;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return LinkedList;
+}());
+exports.LinkedList = LinkedList;
--- Udemy - TypeScript the compelete course/3-sort/src/LinkedList.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/LinkedList.ts
@@ -3,3 +3,38 @@ class Node {
 
   constructor(public data: number) {}
 }
+
+export class LinkedList {
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  } 
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  } 
+}
