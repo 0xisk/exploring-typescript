9dd7b636d639c6217235075e21b0280c885af9b4
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 11:21:14 +0000

    Implementing an Interface Sortable for sorting algorithm, 3-sort.

--- Udemy - TypeScript the compelete course/3-sort/src/Sorter.ts
+++ Udemy - TypeScript the compelete course/3-sort/src/Sorter.ts
@@ -1,6 +1,13 @@
 import { NumbersCollection } from './NumbersCollections'; 
+
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+} 
+
 export class Sorter {
-  constructor(public collection: NumbersCollection) { }
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
