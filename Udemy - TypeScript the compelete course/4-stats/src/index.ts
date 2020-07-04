bce8bef9942160d1dd53af73cdf83faa411ac710
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-04 19:54:39 +0000

    Implementing an analysis, 4-stats.

--- Udemy - TypeScript the compelete course/4-stats/src/index.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/index.ts
@@ -9,4 +9,14 @@ const matches = fs
     return row.split(',');
   });
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
