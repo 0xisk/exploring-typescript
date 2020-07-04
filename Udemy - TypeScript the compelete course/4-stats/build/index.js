2a1725bf5f9ecffe176649a01c973841391e5e4f
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-04 20:04:36 +0000

    Adding a generic categories for win, loss and draw, 4-stats.

--- Udemy - TypeScript the compelete course/4-stats/build/index.js
+++ Udemy - TypeScript the compelete course/4-stats/build/index.js
@@ -12,13 +12,16 @@ var matches = fs_1.default
     .map(function (row) {
     return row.split(',');
 });
+var homeWin = 'H';
+var awayWin = 'A';
+var draw = 'D';
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === homeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === awayWin) {
         manUnitedWins++;
     }
 }
--- Udemy - TypeScript the compelete course/4-stats/src/index.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/index.ts
@@ -9,12 +9,16 @@ const matches = fs
     return row.split(',');
   });
 
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
     manUnitedWins++;
   }
 }
