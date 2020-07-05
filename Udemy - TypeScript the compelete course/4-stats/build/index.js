16829614be69d9921ea7993d5e8adae20c204b60
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-05 20:31:01 +0000

    Implementing a class for reading CSV files, 4-stats

--- Udemy - TypeScript the compelete course/4-stats/build/index.js
+++ Udemy - TypeScript the compelete course/4-stats/build/index.js
@@ -1,17 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync('football.csv', {
-    encoding: 'utf-8'
-})
-    .split("\n")
-    .map(function (row) {
-    return row.split(',');
-});
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// enum - enumerati on
+// Its primary goal is to signal to other engineers that these are all closely related values.
 var MatchResult;
 (function (MatchResult) {
     MatchResult["HomeWin"] = "H";
@@ -19,16 +12,13 @@ var MatchResult;
     MatchResult["Draw"] = "D";
 })(MatchResult || (MatchResult = {}));
 ;
-var homeWin = 'H';
-var awayWin = 'A';
-var draw = 'D';
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === homeWin) {
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === awayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- Udemy - TypeScript the compelete course/4-stats/src/index.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/index.ts
@@ -1,15 +1,9 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-// enum - enumeration
+// enum - enumerati on
 // Its primary goal is to signal to other engineers that these are all closely related values.
 enum MatchResult {
   HomeWin = 'H', 
@@ -19,7 +13,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
