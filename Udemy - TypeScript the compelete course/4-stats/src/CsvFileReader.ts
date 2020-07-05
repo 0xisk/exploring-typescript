405e79f691b77e1bc98f34cd3f3e1a060309ea0d
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-05 21:06:22 +0000

    Implementing a tuple of MatchData Type, 4-stats

--- Udemy - TypeScript the compelete course/4-stats/src/CsvFileReader.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/CsvFileReader.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { dateStringToDate } from './utils'
 import { MatchResult } from './MatchResult';;
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
   
   constructor(public filename: string) {}
 
@@ -16,9 +18,9 @@ export class CsvFileReader {
     .map((row: string): string[] => {
       return row.split(',');
     })
-    .map((row: string[]): any => {
+    .map((row: string[]): MatchData => {
       return [
-        dateStringToDate(row[0]),
+        dateStringToDate(row[0]), 
         row[1],
         row[2],
         parseInt(row[3]),
