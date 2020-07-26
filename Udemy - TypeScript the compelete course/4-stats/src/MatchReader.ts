c98ab6c675c7c22a4a2baa7a01e351c7e81f0155
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-26 15:30:23 +0000

    Refactoring CsvFileReader with Interface-Based approach, Stat

--- Udemy - TypeScript the compelete course/4-stats/src/MatchReader.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/MatchReader.ts
@@ -1,3 +1,8 @@
+import { dateStringToDate } from './utils'
+import { MatchResult } from './MatchResult';;
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
   read(): void;
   data: string[][];
@@ -5,4 +10,16 @@ interface DataReader {
 
 export class MatchReader {
   constructor(public reader: DataReader) {}
+
+  .map((row: string[]): MatchData => {
+    return [
+      dateStringToDate(row[0]), 
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6]
+    ];
+  });
 }
