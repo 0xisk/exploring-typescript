ddf9ed6afba76da37f02e1c41926665032009309
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-26 15:33:20 +0000

    Implement load() fn into MatchReader, Stat

--- Udemy - TypeScript the compelete course/4-stats/src/MatchReader.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/MatchReader.ts
@@ -9,17 +9,24 @@ interface DataReader {
 }
 
 export class MatchReader {
-  constructor(public reader: DataReader) {}
+  matches: MatchData[] = [];
 
-  .map((row: string[]): MatchData => {
-    return [
-      dateStringToDate(row[0]), 
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
-  });
+  constructor(public reader: DataReader) { }
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
+  }
 }
