cdf46d7166d2d26565816b2780363a019d231c50
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-26 15:38:37 +0000

    Updating Reader References, Stat

--- Udemy - TypeScript the compelete course/4-stats/build/CsvFileReader.js
+++ Udemy - TypeScript the compelete course/4-stats/build/CsvFileReader.js
@@ -18,8 +18,7 @@ var CsvFileReader = /** @class */ (function () {
             .split("\n")
             .map(function (row) {
             return row.split(',');
-        })
-            .map(this.mapRow);
+        });
     };
     return CsvFileReader;
 }());
--- Udemy - TypeScript the compelete course/4-stats/build/index.js
+++ Udemy - TypeScript the compelete course/4-stats/build/index.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
-var reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
+var CsvFileReader_1 = require("./CsvFileReader");
+// Create an object that statisfies the 'Data Reader' interface
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass in sth statisfying
+// the 'DataReader' interface
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
     var match = _a[_i];
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
