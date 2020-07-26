210de053e5c574e27fd702264d43542d90c8d30f
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-26 14:56:01 +0000

    Practicing generics, Stats

--- Udemy - TypeScript the compelete course/4-stats/src/MatchResult.ts
+++ Udemy - TypeScript the compelete course/4-stats/src/MatchResult.ts
@@ -1,4 +1,4 @@
-// enum - enumerati on
+// enum - enumeration
 // Its primary goal is to signal to other engineers that these are all closely related values.
 export enum MatchResult {
   HomeWin = 'H', 
---
+++ Udemy - TypeScript the compelete course/4-stats/src/practice-generics.ts
@@ -0,0 +1,48 @@
+// NOTHING TO DO WITH GENERICS
+
+const addOne = (a: number): number => {
+  return a + 1; 
+}
+
+const addTwo = (a: number): number => {
+  return a + 1; 
+}
+
+const addThree = (a: number): number => {
+  return a + 1; 
+}
+
+
+// Best solution
+const add = (a: number, b: number): number => {
+  return a + b;
+}
+
+///////////////////////////
+
+class HoldNumber {
+  data: number;
+}
+
+class HoldString {
+  data: string;
+}
+
+const holdNumber = new HoldNumber();
+holdNumber.data = 123;
+
+const holdString = new HoldString();
+holdString.data = 'Hello'
+
+// Using Generics
+class HoldAnything<TypeOfData> {
+  data: TypeOfData;
+}
+
+const holdNumber2 = new HoldAnything<number>();
+holdNumber2.data = 123;
+
+const holdString2 = new HoldAnything<string>();
+holdString2.data = 'Hello Again!'
+
+///////////////////////////
