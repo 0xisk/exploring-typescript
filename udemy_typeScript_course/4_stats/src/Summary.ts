c7998670052d6e70ee880c540db62049600f1b81
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-08-07 10:47:03 +0000

    Implementing a Summary Class

---
+++ udemy_typeScript_course/4_stats/src/Summary.ts
@@ -0,0 +1,16 @@
+import { MatchData } from "./MatchData";
+
+export interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+}
+
+new Summary(new WinsAnalysis(), new ConsoleReport());
