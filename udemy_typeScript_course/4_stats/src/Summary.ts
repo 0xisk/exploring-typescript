b51d2f9f49d14cf9d1b42cfd2f179d87baf6b84d
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-08-07 11:15:32 +0000

    Implementing buildAndPrintReport inside Summary class

--- udemy_typeScript_course/4_stats/src/Summary.ts
+++ udemy_typeScript_course/4_stats/src/Summary.ts
@@ -11,6 +11,10 @@ export interface OutputTarget {
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
 
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
 
-new Summary(new WinsAnalysis(), new ConsoleReport());
+//new Summary(new WinsAnalysis(), new ConsoleReport());
